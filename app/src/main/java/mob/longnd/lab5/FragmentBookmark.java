4416bf37779fc7c5b95e14c2a9cdf245fbaf360e
longtaru <longhlvt123@gmail.com>
2024-12-01 21:25:24 +0700

    intinal commit

---
+++ app/src/main/java/mob/longnd/lab5/FragmentBookmark.java
@@ -0,0 +1,28 @@
+package mob.longnd.lab5;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentBookmark extends Fragment {
+
+    public static FragmentBookmark newInstance() {
+        FragmentBookmark fragment = new FragmentBookmark();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frm_bookmark, container, false);
+    }
+}
---
+++ app/src/main/java/mob/longnd/lab5/FragmentSettings.java
@@ -0,0 +1,28 @@
+package mob.longnd.lab5;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentSettings extends Fragment {
+
+    public static FragmentSettings newInstance() {
+        FragmentSettings fragment = new FragmentSettings();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frm_settings, container, false);
+    }
+}
